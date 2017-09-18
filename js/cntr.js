28dbc2df8767cf63a869b2cd099304b78b269cba
Iuliia <iuliia@lepistina.com>
2017-09-17 20:15:29 -0700

    Creating to-do list

---
+++ js/cntr.js
@@ -0,0 +1,32 @@
+app.controller('myCtrl', function($scope) {
+    $scope.myData = requestData();
+
+	//requestData();
+	function requestData() {
+		//$http.hfjf instead
+		return [
+	    	{ title : "Reading a book",
+	    	  check: false },
+	    	{ title : "Riding a bike",
+	    	  check: false },
+	    	{ title : "Baking a pie",
+	    	  check: false }
+		];
+	}
+	$scope.upClick = function(parentI) {
+		//var parentI = $(this).parent().attr("id");
+		swap(-1, parentI);
+	};
+	
+	$scope.downClick = function(parentI) {
+		//var parentI = $(this).parent().attr("id");
+		swap(1, parentI);
+	}
+	function swap(operation, id) {
+		var new_Pos_Index = id + operation;
+		var temp_Value = $scope.myData[new_Pos_Index];
+		$scope.myData[new_Pos_Index] = $scope.myData[id];
+		$scope.myData[id] = temp_Value;
+		//updateServer();
+	} 
+});

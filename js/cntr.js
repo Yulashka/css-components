f6ca142e9b0ef5b41127f323d691034c6e7ee75a
Iuliia <iuliia@lepistina.com>
2017-09-18 22:31:20 -0700

    checkbox

--- js/cntr.js
+++ js/cntr.js
@@ -8,21 +8,29 @@ app.controller('myCtrl', function($scope) {
 	    	{ title : "Reading a book",
 	    	  check: false },
 	    	{ title : "Riding a bike",
-	    	  check: false },
+	    	  check: true },
 	    	{ title : "Baking a pie",
 	    	  check: false }
 		];
 	}
 	$scope.upClick = function(parentI) {
 		//var parentI = $(this).parent().attr("id");
-		swap(-1, parentI);
+		if(parentI == 0){
+			console.log("Can't go up");
+		}else{
+			swap(-1, parentI);
+		}
 	};
 	
 	$scope.downClick = function(parentI) {
 		//var parentI = $(this).parent().attr("id");
-		swap(1, parentI);
+		if(parentI == ($scope.myData.length - 1) ){
+			console.log("Can't go down");
+		}else{
+			swap(1, parentI);
+		}
 	};
-	
+
 	function swap(operation, id) {
 		var new_Pos_Index = id + operation;
 		var temp_Value = $scope.myData[new_Pos_Index];
@@ -30,4 +38,13 @@ app.controller('myCtrl', function($scope) {
 		$scope.myData[id] = temp_Value;
 		//updateServer();
 	} 
+
+	//update checkbox
+	$scope.onCheck = function(index) {
+		var title = $scope.myData[index].title;
+		var checkbox = $scope.myData[index].check;
+		console.log(title + " " + checkbox);
+	}
+		
+	
 });

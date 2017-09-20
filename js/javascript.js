d57e92611eeb4b1235d0cc5a783249005e6f21f9
Iuliia <iuliia@lepistina.com>
2017-09-19 21:07:49 -0700

    scrollTop

--- js/javascript.js
+++ js/javascript.js
@@ -294,6 +294,27 @@ $(function() {
 			}
 		});
 	}
+
+	/*scroll-top button*/
+	var scrollTopButton = $(".scroll-top");
+
+	$(window).scroll(function() {
+		var scrollTopPosition = $(this).scrollTop();
+		if(scrollTopPosition > 400){
+			$(scrollTopButton).removeClass("d-none");
+		} else {
+			$(scrollTopButton).addClass("d-none");
+		}
+	}); 
+
+	//scroll up on click
+	$(scrollTopButton).on('click', function() {
+		//animate onclick
+		$("html, body").animate({
+	      scrollTop: 0
+	    }, 1000, "swing" );
+	});
+
 	
 
 });   

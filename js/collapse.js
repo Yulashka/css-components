efc0c855b272a9f92914dbe1a007bd500ce81c74
Iuliia <iuliia@lepistina.com>
2017-11-07 17:24:09 -0800

    fixed carousel javascript

--- js/collapse.js
+++ js/collapse.js
@@ -13,4 +13,30 @@ app.controller('collapseCtrl', function($scope, ) {
 			$("#bakery-web .collapsed").addClass("d-none");
 		}
 	});
+
+	/*Carousel*/
+	var images = ["img/bakery/carousel-pic1.png", "img/bakery/carousel-pic2.png", "img/bakery/carousel-pic3.png", "img/bakery/carousel-pic2.png", "img/bakery/carousel-pic3.png", "img/bakery/carousel-pic1.png"];
+	var imagesElement = $("#bakery-web .image");
+	var endArray = images.slice(3, 7);
+
+	//display images function
+	function addBakeryContent(arr) {
+		for(var i = 0; i < arr.length; i++) {
+			$(imagesElement[i]).attr("src", arr[i]);
+		}
+	}
+	
+	//default display
+	addBakeryContent(images);
+
+	//click right
+	$("#bakery-web .fa-chevron-right").on("click", function() {
+		addBakeryContent(endArray);
+	});
+
+	//click left
+	$("#bakery-web .fa-chevron-left").on("click", function() {
+		addBakeryContent(images);
+	});
+
 });

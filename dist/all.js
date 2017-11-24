b33f06dcb89fb291972e19d2ea648b9407e845ce
Iuliia <iuliia@lepistina.com>
2017-11-24 15:56:03 -0800

    Modal component

--- dist/all.js
+++ dist/all.js
@@ -342,67 +342,7 @@ app.controller('collapseCtrl', function($scope, ) {
 });
 
 
-/*Modal*/
-app.controller('modalCtrl', function($scope, ) {
-	setTimeout(function(){
-	  modal();
-	}, 1000);
-	function modal(){
-		
-		$(".modal").removeClass("d-none");
-	  	$(".css-compon").addClass("modal-bg");
-	  	$(".submit-btn").on("click", function() {
-	  		var email  = $("#email").val();
-	  		var name  = $("#name").val();
-	  		checkEmail(email);
-	  		checkName(name);
-	  	});
-	}
 
-	$(".modal-btn").on('click', function() {
-		$(".modal").addClass("d-none");
-		$(".css-compon").removeClass("modal-bg");
-	});
-
-	function checkEmail(x) {
-		if(x == 0){
-			$("#email-message").text("Dont leave it empty");
-		} else {
-			validate(x);
-		}
-	}
-
-	function checkName(y) {
-		if(y == 0){
-			$("#name-message").text("Dont leave it empty");
-		} else {
-			$("#name-message").text("Valid!");
-		}
-	}
-
-	function validate(x) {
-		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-		if(!emailReg.test(x)) {
-	        $("#email-message").text("Not valid input");
-	       
-	    } else {
-	    	$("#email-message").text("Valid");
-	    	messageSucces();
-	    }
-	}
-
-	function messageSucces(){
-		$(".modal-header").addClass('d-none');
-		$(".modal-body").addClass('d-none');
-		$(".success").removeClass('d-none');
-	}
-
-	function messageError(){
-		$(".modal-header").addClass('d-none');
-		$(".modal-body").addClass('d-none');
-		$(".error").removeClass('d-none');
-	}
-});
 
 /*Scrollspy*/
 //scroll on click
@@ -823,6 +763,65 @@ function closeNav() {
 }
 
 	
+/*Modal*/
+app.controller('modalCtrl', function($scope, ) {
+	setTimeout(function(){
+	  modal();
+	}, 1000);
+	function modal(){
+		
+		$(".modal").removeClass("d-none");
+	  	$(".submit-btn").on("click", function() {
+	  		var email  = $("#email").val();
+	  		var name  = $("#name").val();
+	  		checkEmail(email);
+	  		checkName(name);
+	  	});
+	}
+
+	$(".modal-btn").on('click', function() {
+		$(".modal").addClass("d-none");
+	});
+
+	function checkEmail(x) {
+		if(x == 0){
+			$("#email-message").text("Dont leave it empty");
+		} else {
+			validate(x);
+		}
+	}
+
+	function checkName(y) {
+		if(y == 0){
+			$("#name-message").text("Dont leave it empty");
+		} else {
+			$("#name-message").text("Valid!");
+		}
+	}
+
+	function validate(x) {
+		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+		if(!emailReg.test(x)) {
+	        $("#email-message").text("Not valid input");
+	       
+	    } else {
+	    	$("#email-message").text("Valid");
+	    	messageSucces();
+	    }
+	}
+
+	function messageSucces(){
+		$(".modal-header").addClass('d-none');
+		$(".modal-body").addClass('d-none');
+		$(".success").removeClass('d-none');
+	}
+
+	function messageError(){
+		$(".modal-header").addClass('d-none');
+		$(".modal-body").addClass('d-none');
+		$(".error").removeClass('d-none');
+	}
+});
 var Pages = [-1, 0, 1, 2, 3];
 $(function() {
 /*Another aproach to building a pagination*/

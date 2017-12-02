b8fe671305d2723d2f429ca60f180dc483e1ec96
Iuliia <iuliia@lepistina.com>
2017-12-01 20:50:40 -0800

    Scroll page

--- dist/all.js
+++ dist/all.js
@@ -340,39 +340,6 @@ app.controller('collapseCtrl', function($scope, ) {
 
 
 
-/*Scrollspy*/
-//scroll on click
-app.controller('scrollCtrl', function($scope, ) {
-	$(".scroll-nav a").on('click', function(e) {
-		var attr = $(this).attr("data-href");
-		var pos = $(attr).offset().top;
-		var navHeight = $(".scroll-nav").innerHeight();
-		var sum = pos - navHeight;
-		//animate onclick
-		$("html, body").animate({
-	      scrollTop: sum
-	    }, 1000, "swing" );
-	    e.preventDefault();
-	});
-
-	$(window).on('scroll', function() {
-		var navHeight = $(".scroll-nav").outerHeight();
-		var scrollie = $(window).scrollTop() + navHeight + 1;
-		var rows = $(".scroll-body .row");
-		for(var i = 0; i < rows.length;  i++){
-			//console.log(i);
-			var startPos = $(rows[i]).offset().top;
-			var rowHeight = $(rows[i]).innerHeight();
-			var endPos = startPos + rowHeight;
-			if( (endPos >= scrollie) && (scrollie >= startPos) ) {
-				var currentAttr = $(rows[i]).attr("id");
-				currentAttr = "#" + currentAttr; 
-				$(".scroll-nav a").removeClass("active-scroll");
-				$("a[data-href =" + "'" + currentAttr + "'" + "]").addClass("active-scroll");
-			}
-		}	
-	});
-});
 
 /*Pagination*/
 /*app.controller('pagination2Ctrl', function($scope, ) {
@@ -881,9 +848,36 @@ app.controller('paginationCtrl', function($scope, ) {
 	}
 });
 
-/*Popovers*/
-app.controller('popoverCtrl', function($scope, ) {
-	$("#popover button").unbind().click(function() {
-		$(".pop-text").toggle();
+/*Scrollspy*/
+//scroll on click
+app.controller('scrollCtrl', function($scope, ) {
+	$(".scroll-nav a").on('click', function(e) {
+		var attr = $(this).attr("data-href");
+		var pos = $(attr).offset().top;
+		var navHeight = $(".scroll-nav").innerHeight();
+		var sum = pos - navHeight;
+		//animate onclick
+		$("html, body").animate({
+	      scrollTop: sum
+	    }, 1000, "swing" );
+	    e.preventDefault();
+	});
+
+	$(window).on('scroll', function() {
+		var navHeight = $(".scroll-nav").outerHeight();
+		var scrollie = $(window).scrollTop() + navHeight + 1;
+		var rows = $(".scroll-body .row");
+		for(var i = 0; i < rows.length;  i++){
+			//console.log(i);
+			var startPos = $(rows[i]).offset().top;
+			var rowHeight = $(rows[i]).innerHeight();
+			var endPos = startPos + rowHeight;
+			if( (endPos >= scrollie) && (scrollie >= startPos) ) {
+				var currentAttr = $(rows[i]).attr("id");
+				currentAttr = "#" + currentAttr; 
+				$(".scroll-nav a").removeClass("active-scroll");
+				$("a[data-href =" + "'" + currentAttr + "'" + "]").addClass("active-scroll");
+			}
+		}	
 	});
 });
--- js/cntr.js
+++ js/cntr.js
@@ -32,39 +32,6 @@ app.controller('collapseCtrl', function($scope, ) {
 
 
 
-/*Scrollspy*/
-//scroll on click
-app.controller('scrollCtrl', function($scope, ) {
-	$(".scroll-nav a").on('click', function(e) {
-		var attr = $(this).attr("data-href");
-		var pos = $(attr).offset().top;
-		var navHeight = $(".scroll-nav").innerHeight();
-		var sum = pos - navHeight;
-		//animate onclick
-		$("html, body").animate({
-	      scrollTop: sum
-	    }, 1000, "swing" );
-	    e.preventDefault();
-	});
-
-	$(window).on('scroll', function() {
-		var navHeight = $(".scroll-nav").outerHeight();
-		var scrollie = $(window).scrollTop() + navHeight + 1;
-		var rows = $(".scroll-body .row");
-		for(var i = 0; i < rows.length;  i++){
-			//console.log(i);
-			var startPos = $(rows[i]).offset().top;
-			var rowHeight = $(rows[i]).innerHeight();
-			var endPos = startPos + rowHeight;
-			if( (endPos >= scrollie) && (scrollie >= startPos) ) {
-				var currentAttr = $(rows[i]).attr("id");
-				currentAttr = "#" + currentAttr; 
-				$(".scroll-nav a").removeClass("active-scroll");
-				$("a[data-href =" + "'" + currentAttr + "'" + "]").addClass("active-scroll");
-			}
-		}	
-	});
-});
 
 /*Pagination*/
 /*app.controller('pagination2Ctrl', function($scope, ) {

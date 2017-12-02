f778c77a6c01bb3280ac12a29795dcc56f4e721b
Iuliia <iuliia@lepistina.com>
2017-12-01 21:53:19 -0800

    Fixing the scroll bug, I had two versions of scroll js

--- dist/all.js
+++ dist/all.js
@@ -543,43 +543,6 @@ app.controller('formCtrl', function($scope, ) {
 //var Pages = [-1, 0, 1, 2, 3];
 
 $(function() {
-	
-
-	
-
-	
-
-	/*Scrollspy*/
-	//scroll on click
-	$(".scroll-nav a").on('click', function(e) {
-		var attr = $(this).attr("data-href");
-		var pos = $(attr).offset().top;
-		var navHeight = $(".scroll-nav").innerHeight();
-		var sum = pos - navHeight;
-		//animate onclick
-		$("html, body").animate({
-	      scrollTop: sum
-	    }, 1000, "swing" );
-	    e.preventDefault();
-	});
-
-	$(window).on('scroll', function() {
-		var navHeight = $(".scroll-nav").outerHeight();
-		var scrollie = $(window).scrollTop() + navHeight + 1;
-		var rows = $(".scroll-body .row");
-		for(var i = 0; i < rows.length;  i++){
-			//console.log(i);
-			var startPos = $(rows[i]).offset().top;
-			var rowHeight = $(rows[i]).innerHeight();
-			var endPos = startPos + rowHeight;
-			if( (endPos >= scrollie) && (scrollie >= startPos) ) {
-				var currentAttr = $(rows[i]).attr("id");
-				currentAttr = "#" + currentAttr; 
-				$(".scroll-nav a").removeClass("active-scroll");
-				$("a[data-href =" + "'" + currentAttr + "'" + "]").addClass("active-scroll");
-			}
-		}	
-	});
 
 	
 	/*Pagination*/
@@ -854,7 +817,8 @@ app.controller('scrollCtrl', function($scope, ) {
 	$(".scroll-nav a").on('click', function(e) {
 		var attr = $(this).attr("data-href");
 		var pos = $(attr).offset().top;
-		var navHeight = $(".scroll-nav").innerHeight();
+		// 40 is the height of the outer Sidenav button
+		var navHeight = $(".scroll-nav").innerHeight() + 40;
 		var sum = pos - navHeight;
 		//animate onclick
 		$("html, body").animate({
@@ -863,9 +827,11 @@ app.controller('scrollCtrl', function($scope, ) {
 	    e.preventDefault();
 	});
 
+	// update menu highlight
 	$(window).on('scroll', function() {
 		var navHeight = $(".scroll-nav").outerHeight();
-		var scrollie = $(window).scrollTop() + navHeight + 1;
+		// 40 is the height of the outer Sidenav button
+		var scrollie = $(window).scrollTop() + navHeight + 41;
 		var rows = $(".scroll-body .row");
 		for(var i = 0; i < rows.length;  i++){
 			//console.log(i);

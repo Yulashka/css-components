a50075506f21b54d03e50f09b055882bae18a1fb
Iuliia <iuliia@lepistina.com>
2017-12-16 19:19:06 -0800

    Wrote components descriptions and fixed accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,18 +1,9 @@
 /*Accordion */
 app.controller('accordCtrl', function($scope, ) {
+	/*accordion*/
 	$("#accordPage .accordion .accord-tab").on("click", function() {
-		$("#accordPage .accord-text").addClass("d-none");
+		$("#accordPage .accordion .accord-text").addClass("d-none");
 		$("#accordPage .accordion .accord-tab").removeClass("a-acc-tab");
-		$(this).addClass("a-acc-tab");
-		var attrAccord = $(this).attr("id");
-		$(attrAccord).removeClass("d-none");
-		$(attrAccord + " i").addClass("a-acc-tab");
-	});
-
-	/*accordion mobile*/
-	$("#accordPage .accordion-mobile .accord-tab").on("click", function() {
-		$("#accordPage .accordion-mobile .accord-text").addClass("d-none");
-		$("#accordPage .accordion-mobile .accord-tab").removeClass("a-acc-tab");
 		$(this).addClass("a-acc-tab");
 		var attrAccord = $(this).attr("id");
 		$(attrAccord).removeClass("d-none");
